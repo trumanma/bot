b57de6160fac5a4538b6832384fef3c555211993
trumanma <87671339+trumanma@users.noreply.github.com>
2021-09-17 23:20:40 +0300

    Add files via upload

---
+++ src/test/java/com/github/trumanma/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.trumanma.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
